c18e48236cb2acab604c815e9c1c9d97e9689be6
Rinat Ussenov <rinat.ussenov@gmail.com>
2018-04-10 19:13:27 -0400

    Fixes GA script

--- pages/_document.js
+++ pages/_document.js
@@ -15,38 +15,34 @@ export default class Skubana extends Document {
 
         return (
             <html>
-            <Head>
-                <meta charSet='utf-8' />
-                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-                <link href="/static/css/font-awesome.css" rel="stylesheet"/>
-                <link rel="stylesheet" id="wpb-google-fonts-css"
-                      href="/static/css/openSans.css"
-                      type="text/css" media="all"/>
-            </Head>
-            <body>
-            <Main/>
-            <NextScript/>
-            </body>
-            <script dangerouslySetInnerHTML={{
-                __html: `
-                        (function (i, s, o, g, r, a, m) {
-                                i['GoogleAnalyticsObject'] = r;
-                                i[r] = i[r] || function () {
-                                        (i[r].q = i[r].q || []).push(arguments)
-                                    }, i[r].l = 1 * new Date();
-                                a = s.createElement(o),
-                                    m = s.getElementsByTagName(o)[0];
-                                a.async = 1;
-                                a.src = g;
-                                m.parentNode.insertBefore(a, m)
-                            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+                <Head>
+                    <meta charSet='utf-8'/>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                    <link href="/static/css/font-awesome.css" rel="stylesheet"/>
+                    <link rel="stylesheet" id="wpb-google-fonts-css"
+                          href="/static/css/openSans.css"
+                          type="text/css" media="all"/>
+                </Head>
+                <body>
+                    <Main/>
+                    <NextScript/>
+                    <script dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','UA-36561446-2');
+                      window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
 
-                           ga('create', 'UA-36561446-2', 'auto');
-                           ga('send', 'pageview');
-                `
-            }}/>
+                        gtag('config', 'UA-36561446-2');
+                      `
+                    }}/>
+                    <noscript
+                        dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}}/>
+                </body>
             </html>
         )
     }
 }
-
